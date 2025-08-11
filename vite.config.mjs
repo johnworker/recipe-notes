1310662e7ea2dbaf6ca43505c0a865ce738ab1a8
hunte <besscomdark@gmail.com>
2025-08-11 21:24:55 +0800

    發佈專案

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] // ex: "recipe-notes"
 const isCI = !!process.env.GITHUB_ACTIONS
 
 export default defineConfig({
-  base: isCI && repoName ? `/${repoName}/` : '/', // CI 自動帶入 /recipe-notes/
+  // 在 GitHub Actions 內自動使用 /{repo}/ 作為 base；本機為 "/"
+  base: isCI && repo ? `/${repo}/` : '/',
   plugins: [react()],
 })
