1fc1c55025cb93215a010b094247a384de8ae275
hunte <besscomdark@gmail.com>
2025-08-11 20:33:49 +0800

    發佈專案

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GH Pages 會有這個環境變數，例如 "johnworker/recipe-notes"
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
 const isCI = !!process.env.GITHUB_ACTIONS
 
 export default defineConfig({
-  base: isCI && repoName ? `/${repoName}/` : '/',  // GH Pages 自動帶 /recipe-notes/
+  base: isCI && repoName ? `/${repoName}/` : '/', // CI 自動帶入 /recipe-notes/
   plugins: [react()],
 })
